11f840b08faf43d70a375f5f02bd6f7571ead111
Vincent Marschalek <MAR220537@spengergasse.at>
2024-01-05 18:13:17 +0100

    initial_commit

---
+++ src/main/java/at/alphaplan/AlphaWeb/AlphaWebApplication.java
@@ -0,0 +1,13 @@
+package at.alphaplan.AlphaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/alphaplan/AlphaWeb/AlphaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package at.alphaplan.AlphaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
